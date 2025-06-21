feaa41c67093d7a587d90e544285a1aaba95d6e0
KanatiMC <84251013+KanatiMC@users.noreply.github.com>
2025-06-21 17:47:05 -0500

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  licenseKey: process.env.LICENSE || "",
+  prefix: ".",
+  mongo: process.env.MONGO || "",
+  applicationId: process.env.APP_ID || "1303719957124616294",
+  token: process.env.TOKEN || "",
+  lastfmUser: process.env.LASTFM_USER || null,
+  lastfmKey: process.env.LASTFM_API_KEY || null,
+  spotifyClientId: "94917338b152485085702286a4fb73ca",
+  spotifyClientSecret: "dc5b1d33558c483786c3fceca384630b",
+};
